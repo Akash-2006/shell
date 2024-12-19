e8a35b61ab6b01d8d90cc42c5ec3c201cd769fa8
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2024-12-19 17:10:09 +0530

    created shell.js

---
+++ powershell.js
@@ -0,0 +1,91 @@
+let currentDirectory = ['~'];
+const promptMessage = 'singuluriakash@Singuluris-MacBook';
+const fileLocations = ['/git', '/calculator', '/arrays'];
+
+const git = function () {
+  return 'this is git file';
+};
+
+const calculator = function () {
+  return 'this is caluculator';
+};
+
+const arrays = function () {
+  return 'this is array';
+};
+
+const filesData = [git, calculator, arrays];
+
+const changeDirectory = function (location) {
+  if (location === '..') {
+    currentDirectory.pop();
+    return '';
+  }
+  const file = location;
+
+  if (fileLocations.includes(file)) {
+    currentDirectory = currentDirectory.concat(location);
+    return '';
+  }
+
+  return 'Error : ' + location + ' There is no such directory';
+};
+
+const getMessage = function (args) {
+  return args.join(' ');
+};
+
+const getCurrentDirectory = function () {
+  return currentDirectory;
+};
+
+const printFiles = function () {
+  return fileLocations.join('\n');
+};
+
+const deleteFile = function (fileName) {
+  if (!fileLocations.includes(fileName)) {
+    return 'Error : ' + fileName + ' There is no such directory';
+  }
+
+  const location = fileLocations.indexOf(fileName);
+  fileLocations.splice(location, 1);
+  return '';
+};
+
+const printData = function (fileName) {
+  if (!fileLocations.includes(fileName)) {
+    return 'Error : ' + fileName + ' There is no such directory';
+  }
+
+  const location = fileLocations.indexOf(fileName);
+  return filesData[location]();
+};
+
+const process = function (commandToRun) {
+  const [command, ...args] = commandToRun.split(' ');
+  switch (command) {
+    case 'cd':
+      return changeDirectory(...args);
+    case 'echo':
+      return getMessage(args);
+    case 'pwd':
+      return getCurrentDirectory();
+    case 'ls':
+      return printFiles();
+    case 'rm':
+      return deleteFile(...args);
+    case 'cat':
+      return printData(...args);
+    default:
+      return command + ' command not found';
+  }
+};
+
+while (true) {
+  const commandToRun = prompt(promptMessage + currentDirectory.join('') + ' % ');
+  const result = process(commandToRun);
+  if (result !== '') {
+    console.log(result);
+  }
+}
